c34e7fe8fd020a0fc5837544cf7dd779585573ea
Harikrushna <harikrushnappatel@gmail.com>
2024-11-01 16:55:38 +0530

    text to image

---
+++ pipeline/text-to-image/Freepik_flux.1-lite-8B-alpha/app.py
@@ -0,0 +1,30 @@
+import torch
+from diffusers import FluxPipeline
+
+base_model_id = "Freepik/flux.1-lite-8B-alpha"
+torch_dtype = torch.bfloat16
+device = "cuda"
+
+# Load the pipe
+model_id = "Freepik/flux.1-lite-8B-alpha"
+pipe = FluxPipeline.from_pretrained(
+    model_id, torch_dtype=torch_dtype
+).to(device)
+
+# Inference
+prompt = "A close-up image of a green alien with fluorescent skin in the middle of a dark purple forest"
+
+guidance_scale = 3.5
+n_steps = 28
+seed = 11
+
+with torch.inference_mode():
+    image = pipe(
+        prompt=prompt,
+        generator=torch.Generator(device="cpu").manual_seed(seed),
+        num_inference_steps=n_steps,
+        guidance_scale=guidance_scale,
+        height=1024,
+        width=1024,
+    ).images[0]
+image.save("output.png")
